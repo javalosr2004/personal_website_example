5fd8dee99832dd59b7183ca6b4cc352bad3e398f
javalosr2004 <jesusavalosr2004@gmail.com>
2023-08-04 18:10:39 -0700

    attempting suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from 'react'
 import Carousel from './components/layout/Carousel'
 import Image from 'next/image'
 
@@ -9,41 +10,45 @@ export default function HomePage() {
             </h1>
             <div className="w-full flex flex-row">
                 <div className="relative overflow-hidden border-black border-2 w-[200px] h-[200px] rounded-[50%] my-4">
-                    <Image
-                        src={'/avatar.jpg'}
-                        alt="Picture of Jesus Avalos"
-                        fill={true}
-                        style={{ objectFit: 'cover' }}
-                        draggable={false}
-                    ></Image>
+                    <Suspense fallback={'Loading...'}>
+                        <Image
+                            src={'/avatar.jpg'}
+                            alt="Picture of Jesus Avalos"
+                            fill={true}
+                            style={{ objectFit: 'cover' }}
+                            draggable={false}
+                        ></Image>
+                    </Suspense>
                 </div>
                 <div className="pt-4">
                     <h1>hello</h1>
                 </div>
             </div>
-            <h1 className="mt-10 underline underline-offset-1">Experience:</h1>
+            <h1 className="mt-10 underline underline-offset-1">Past Work:</h1>
             <div className="flex flex-1 flex-col items-center justify-center">
                 <h1 className="mt-5 md:text-xl font-bold">
                     Hospice of SLO Website
                 </h1>
                 <div className="relative mt-8 w-full h-auto md:max-w-[67rem] p-4 ">
-                    <Carousel
-                        images={[
-                            'discussion_1.png',
-                            'select_2.png',
-                            'signup_1.png',
-                            'complete_signup_1.png',
-                            'delete_shift_1.png',
-                        ]}
-                        alt={[
-                            'Discussion',
-                            'Selection',
-                            'Signup Form',
-                            'Completed Signup',
-                            'Delete Shift',
-                        ]}
-                        path="/hospice_of_slo"
-                    ></Carousel>
+                    <Suspense fallback={'loading...'}>
+                        <Carousel
+                            images={[
+                                'discussion_1.png',
+                                'select_2.png',
+                                'signup_1.png',
+                                'complete_signup_1.png',
+                                'delete_shift_1.png',
+                            ]}
+                            alt={[
+                                'Discussion',
+                                'Selection',
+                                'Signup Form',
+                                'Completed Signup',
+                                'Delete Shift',
+                            ]}
+                            path="/hospice_of_slo"
+                        ></Carousel>
+                    </Suspense>
                 </div>
             </div>
         </div>
