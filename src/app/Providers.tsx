ba8829269764d24b95a4c3385c320b371ba4d3f4
javalosr2004 <jesusavalosr2004@gmail.com>
2023-08-07 19:10:21 -0700

    server fix?

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -3,14 +3,11 @@ import React from 'react'
 import { Provider } from 'react-redux'
 import { SessionProvider } from 'next-auth/react'
 import { store } from '@/store'
-import TransitionNav from './components/layout/TransitionNav'
 
 export default function Providers({ children }: { children: React.ReactNode }) {
     return (
         <Provider store={store}>
-            <SessionProvider>
-                <TransitionNav>{children}</TransitionNav>
-            </SessionProvider>
+            <SessionProvider>{children}</SessionProvider>
         </Provider>
     )
 }
