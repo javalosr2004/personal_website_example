5358a8cc9f1c94d25275423368e218bf49139cd4
javalosr2004 <jesusavalosr2004@gmail.com>
2023-08-10 17:27:09 -0700

    abstracted multiple step form with switch statements

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -13,10 +13,170 @@ import {
 } from '@/components/ui/dialog'
 
 import { Calendar } from '@/components/ui/calendar'
-
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
+
+import { State, Action } from './sessionReducer/typings'
+
+const FirstDialog = ({
+    state,
+    dispatch,
+}: {
+    state: State
+    dispatch: React.Dispatch<Action>
+}) => {
+    return (
+        <div className="grid gap-4 py-4">
+            <div className={style.form_group}>
+                <Label htmlFor="title" className="text-right">
+                    Title
+                </Label>
+                <Input
+                    id="title"
+                    value={state.title}
+                    onChange={(event) =>
+                        dispatch({
+                            type: 'set-title',
+                            payload: event.currentTarget.value,
+                        })
+                    }
+                    placeholder="Google"
+                />
+            </div>
+            <div className={style.form_group}>
+                <Label htmlFor="date" className="text-right">
+                    Date
+                </Label>
+                <div className="h-[350px] w-[280px]">
+                    <Calendar
+                        id="date"
+                        mode="range"
+                        selected={state.date}
+                        onSelect={(selected) =>
+                            dispatch({
+                                type: 'set-date',
+                                payload: selected,
+                            })
+                        }
+                    ></Calendar>
+                </div>
+            </div>
+            <div className={style.form_group}>
+                <Label htmlFor="simple_description" className="text-right">
+                    Simple Description
+                </Label>
+                <Input
+                    id="description"
+                    value={state.simple_description}
+                    onChange={(event) =>
+                        dispatch({
+                            type: 'set-simple-description',
+                            payload: event.currentTarget.value,
+                        })
+                    }
+                    placeholder="Optimized code."
+                />
+            </div>
+        </div>
+    )
+}
+
+const SecondDialog = ({
+    state,
+    dispatch,
+}: {
+    state: State
+    dispatch: React.Dispatch<Action>
+}) => {
+    return (
+        <div className="grid gap-4 py-4">
+            <div className={style.form_group_larger}>
+                <Label htmlFor="description" className="text-right">
+                    Description
+                </Label>
+                <Input
+                    id="description"
+                    value={state.detailed_description}
+                    onChange={(event) =>
+                        dispatch({
+                            type: 'set-detailed-description',
+                            payload: event.currentTarget.value,
+                        })
+                    }
+                    placeholder="Google"
+                />
+            </div>
+            <div className={style.form_group_larger}>
+                <Label htmlFor="root_folder" className="text-right">
+                    Root Folder (optional)
+                </Label>
+                <Input
+                    id="root_folder"
+                    value={state.root_folder}
+                    onChange={(event) =>
+                        dispatch({
+                            type: 'set-root-folder',
+                            payload: event.currentTarget.value,
+                        })
+                    }
+                    placeholder="/rose_pictures"
+                />
+            </div>
+            <div className={style.form_group_larger}>
+                <Label htmlFor="images" className="text-right">
+                    Slideshow Images
+                </Label>
+                <Input
+                    id="images"
+                    value={state.carousel_images}
+                    onChange={(event) =>
+                        dispatch({
+                            type: 'set-carousel-images',
+                            payload: event.currentTarget.value,
+                        })
+                    }
+                    placeholder="['rose_1.png', 'rose_2.png']"
+                />
+            </div>
+            <div className={style.form_group_larger}>
+                <Label htmlFor="alt" className="text-right">
+                    Alt Captions
+                </Label>
+                <Input
+                    id="alt"
+                    value={state.alt}
+                    onChange={(event) =>
+                        dispatch({
+                            type: 'set-alt',
+                            payload: event.currentTarget.value,
+                        })
+                    }
+                    placeholder="['Cool picture', 'Nice dog']"
+                />
+            </div>
+        </div>
+    )
+}
+
+const MultipleStepForm = ({
+    step,
+    state,
+    dispatch,
+}: {
+    step: number
+    state: State
+    dispatch: React.Dispatch<Action>
+}) => {
+    switch (step) {
+        case 0:
+            return <FirstDialog state={state} dispatch={dispatch} />
+        case 1:
+            return <SecondDialog state={state} dispatch={dispatch} />
+        default:
+            return <FirstDialog state={state} dispatch={dispatch} />
+    }
+}
 
 export default function AddExperience() {
     const initialState = {
@@ -50,136 +210,11 @@ export default function AddExperience() {
                             be detailed.
                         </DialogDescription>
                     </DialogHeader>
-                    {state.step == 0 ? (
-                        <div className="grid gap-4 py-4">
-                            <div className={style.form_group}>
-                                <Label htmlFor="title" className="text-right">
-                                    Title
-                                </Label>
-                                <Input
-                                    id="title"
-                                    value={state.title}
-                                    onChange={(event) =>
-                                        dispatch({
-                                            type: 'set-title',
-                                            payload: event.currentTarget.value,
-                                        })
-                                    }
-                                    placeholder="Google"
-                                />
-                            </div>
-                            <div className={style.form_group}>
-                                <Label htmlFor="date" className="text-right">
-                                    Date
-                                </Label>
-                                <div className="h-[350px] w-[280px]">
-                                    <Calendar
-                                        id="date"
-                                        mode="range"
-                                        selected={state.date}
-                                        onSelect={(selected) =>
-                                            dispatch({
-                                                type: 'set-date',
-                                                payload: selected,
-                                            })
-                                        }
-                                    ></Calendar>
-                                </div>
-                            </div>
-                            <div className={style.form_group}>
-                                <Label
-                                    htmlFor="simple_description"
-                                    className="text-right"
-                                >
-                                    Simple Description
-                                </Label>
-                                <Input
-                                    id="description"
-                                    value={state.simple_description}
-                                    onChange={(event) =>
-                                        dispatch({
-                                            type: 'set-simple-description',
-                                            payload: event.currentTarget.value,
-                                        })
-                                    }
-                                    placeholder="Optimized code."
-                                />
-                            </div>
-                        </div>
-                    ) : (
-                        <div className="grid gap-4 py-4">
-                            <div className={style.form_group_larger}>
-                                <Label
-                                    htmlFor="description"
-                                    className="text-right"
-                                >
-                                    Description
-                                </Label>
-                                <Input
-                                    id="description"
-                                    value={state.detailed_description}
-                                    onChange={(event) =>
-                                        dispatch({
-                                            type: 'set-detailed-description',
-                                            payload: event.currentTarget.value,
-                                        })
-                                    }
-                                    placeholder="Google"
-                                />
-                            </div>
-                            <div className={style.form_group_larger}>
-                                <Label
-                                    htmlFor="root_folder"
-                                    className="text-right"
-                                >
-                                    Root Folder (optional)
-                                </Label>
-                                <Input
-                                    id="root_folder"
-                                    value={state.root_folder}
-                                    onChange={(event) =>
-                                        dispatch({
-                                            type: 'set-root-folder',
-                                            payload: event.currentTarget.value,
-                                        })
-                                    }
-                                    placeholder="/rose_pictures"
-                                />
-                            </div>
-                            <div className={style.form_group_larger}>
-                                <Label htmlFor="images" className="text-right">
-                                    Slideshow Images
-                                </Label>
-                                <Input
-                                    id="images"
-                                    value={state.carousel_images}
-                                    onChange={(event) =>
-                                        dispatch({
-                                            type: 'set-carousel-images',
-                                            payload: event.currentTarget.value,
-                                        })
-                                    }
-                                    placeholder="['rose_1.png', 'rose_2.png']"
-                                />
-                            </div>
-                            <div className={style.form_group_larger}>
-                                <Label htmlFor="alt" className="text-right">
-                                    Alt Captions
-                                </Label>
-                                <Input
-                                    id="alt"
-                                    value={state.alt}
-                                    onChange={(event) =>
-                                        dispatch({
-                                            type: 'set-alt',
-                                            payload: event.currentTarget.value,
-                                        })
-                                    }
-                                    placeholder="['Cool picture', 'Nice dog']"
-                                />
-                            </div>
-                        </div>
-                    )}
+                    <MultipleStepForm
+                        step={state.step}
+                        state={state}
+                        dispatch={dispatch}
+                    />
                     <DialogFooter>
                         {state.step - 1 >= 0 && (
                             <Button
