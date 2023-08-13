26b029130a9f9a53ceb6c28962bd8ffcf8e98db0
javalosr2004 <jesusavalosr2004@gmail.com>
2023-08-13 12:00:20 -0700

    prev commit

--- src/app/loading.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Loading() {
-    return (
-        <div className="w-[100vw] text-center">
-            <h1>Loading...</h1>
-        </div>
-    )
-}
