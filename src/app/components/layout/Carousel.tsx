7112a6e43ed2a8966638b698aff36774818c5726
javalosr2004 <jesusavalosr2004@gmail.com>
2023-08-17 13:04:17 -0700

    refomatted way of obtaining static data in experience/[slug]

--- src/app/components/layout/Carousel.tsx
+++ src/app/components/layout/Carousel.tsx
@@ -1,10 +1,11 @@
 'use client'
 
-import React, { useCallback, useEffect, useState } from 'react'
-import { PrevButton, NextButton } from './emblaButtons'
-import useEmblaCarousel, { EmblaCarouselType } from 'embla-carousel-react'
+import React from 'react'
 import Image from 'next/image'
+
 import style from './carousel.module.css'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Navigation } from 'swiper/modules'
 
 export default function Carousel({
     images,
@@ -15,87 +16,76 @@ export default function Carousel({
     alt: string[]
     path?: string
 }) {
-    const [emblaRef, emblaAPI] = useEmblaCarousel({
-        containScroll: 'trimSnaps',
-    })
-    const [prevBtnDisabled, setPrevBtnDisabled] = useState(true)
-    const [nextBtnDisabled, setNextBtnDisabled] = useState(true)
-    const [currentIndex, setCurrentIndex] = useState(0)
-
-    // you can use one liners, if you want
-    const scrollPrev = useCallback(() => {
-        if (!emblaAPI) return
-        requestAnimationFrame(() => {
-            emblaAPI.scrollPrev()
-        })
-    }, [emblaAPI])
-
-    const scrollNext = useCallback(() => {
-        if (!emblaAPI) return
-        requestAnimationFrame(() => {
-            emblaAPI.scrollNext()
-        })
-    }, [emblaAPI])
-
-    const handleSelect = (emblaAPI: EmblaCarouselType) => {
-        if (!emblaAPI) return
-        setNextBtnDisabled(!emblaAPI.canScrollNext())
-        setPrevBtnDisabled(!emblaAPI.canScrollPrev())
-        setCurrentIndex(emblaAPI.selectedScrollSnap())
-    }
-
-    // const handleImageClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    //     // e.currentTarget.classList.add('scale-[200%]')
-    //     // The coordinates relative to the viewport
-    // }
-
-    useEffect(() => {
-        if (!emblaAPI) return
-        handleSelect(emblaAPI)
-        emblaAPI?.on('select', handleSelect)
-    }, [emblaAPI])
-
     return (
-        <>
-            <div className={style.embla}>
-                <div className={style.embla__viewport} ref={emblaRef}>
-                    <div className={style.embla__container}>
-                        {images.map((image_url) => {
-                            const complete_path: string = path
-                                ? path + '/' + image_url
-                                : image_url
-                            return (
-                                <div
-                                    // onClick={handleImageClick}
-                                    key={image_url}
-                                    className={`${style.embla__slide} z-10`}
-                                >
-                                    <Image
-                                        src={complete_path}
-                                        alt=""
-                                        fill={true}
-                                        sizes="100vw"
-                                        style={{ objectFit: 'scale-down' }}
-                                        quality={100}
-                                    ></Image>
-                                </div>
-                            )
-                        })}
-                    </div>
-                </div>
-                {/* <div className={style.embla__buttons}> */}
-                <PrevButton
-                    className={`${style.embla__button} absolute top-[50%] left-0`}
-                    onClick={scrollPrev}
-                    disabled={prevBtnDisabled}
-                ></PrevButton>
-                <NextButton
-                    className={`${style.embla__button} absolute top-[50%] right-0`}
-                    onClick={scrollNext}
-                    disabled={nextBtnDisabled}
-                ></NextButton>
-                <h1 className="underline">{alt[currentIndex] || ''}</h1>
-            </div>
-        </>
+        <Swiper
+            navigation={true}
+            modules={[Navigation]}
+            className={style.mySwiper}
+        >
+            {images.map((image_url, idx) => {
+                const complete_path: string = path
+                    ? path + '/' + image_url
+                    : image_url
+                return (
+                    <SwiperSlide
+                        className="w-[400px] h-[500px]"
+                        key={image_url}
+                    >
+                        <Image
+                            src={complete_path}
+                            alt={alt[idx] || ''}
+                            fill={true}
+                            sizes="100vw"
+                            style={{ objectFit: 'scale-down' }}
+                            quality={100}
+                        ></Image>
+                    </SwiperSlide>
+                )
+            })}
+        </Swiper>
     )
+    // return (
+    //     <>
+    //         <div className={style.embla}>
+    //             <div className={style.embla__viewport} ref={emblaRef}>
+    //                 <div className={style.embla__container}>
+    //                     {images.map((image_url) => {
+    //                         const complete_path: string = path
+    //                             ? path + '/' + image_url
+    //                             : image_url
+    //                         return (
+    //                             <div
+    //                                 // onClick={handleImageClick}
+    //                                 key={image_url}
+    //                                 className={`${style.embla__slide} z-10`}
+    //                             >
+    //                                 <Image
+    //                                     src={complete_path}
+    //                                     alt=""
+    //                                     fill={true}
+    //                                     sizes="100vw"
+    //                                     style={{ objectFit: 'scale-down' }}
+    //                                     quality={100}
+    //                                 ></Image>
+    //                             </div>
+    //                         )
+    //                     })}
+    //                 </div>
+    //             </div>
+    //             {/* <div className={style.embla__buttons}> */}
+    //             <PrevButton
+    //                 className={`${style.embla__button} absolute top-[50%] left-0`}
+    //                 onClick={scrollPrev}
+    //                 disabled={prevBtnDisabled}
+    //             ></PrevButton>
+    //             <NextButton
+    //                 className={`${style.embla__button} absolute top-[50%] right-0`}
+    //                 onClick={scrollNext}
+    //                 disabled={nextBtnDisabled}
+    //             ></NextButton>
+    //             <h1 className="underline">{alt[currentIndex] || ''}</h1>
+    //         </div>
+    //     </>
+
+    // )
 }
--- src/app/experience/[slug]/page.tsx
+++ src/app/experience/[slug]/page.tsx
@@ -1,11 +1,14 @@
 import EditExperience from '@/app/components/experience/EditExperience'
 import Carousel from '@/app/components/layout/Carousel'
-import { experienceType } from '@/typings/modelTypes'
+import { ExperienceState } from '@/store/experienceState'
+
 import React from 'react'
 
 export async function generateStaticParams() {
     const DB_URL: string = (process.env.DB_API || '') + '/experiences'
-    const experiences: experienceType[] = await fetch(DB_URL, { method: 'GET' })
+    const experiences: ExperienceState[] = await fetch(DB_URL, {
+        method: 'GET',
+    })
         .then((data) => data.json())
         .catch((err) => {
             throw Error(err)
@@ -13,27 +16,25 @@ export async function generateStaticParams() {
 
     return experiences.map((experience) => ({
         slug: experience.slug,
+        experience: experience,
     }))
 }
 
 export default async function ExperiencePage({
     params,
 }: {
-    params: { slug: string }
+    params: { slug: string; experience: ExperienceState }
 }) {
     const DB_URL: string = (process.env.DB_API || '') + '/experiences'
-    console.log('slug ', params.slug)
-    const res = await fetch(`${DB_URL}/${params.slug}`)
+    const experiences: ExperienceState[] = await fetch(DB_URL, {
+        method: 'GET',
+    }).then((data) => data.json())
 
-    if (!res.ok) {
-        return (
-            <div className="w-[90vw] text-center">
-                <h1>Blank page. lol.</h1>
-                <p>{res.statusText}</p>
-            </div>
-        )
-    } else {
-        const experience = (await res.json()) as experienceType
+    const experience = experiences.find(
+        (experience) => experience.slug === params.slug
+    )
+
+    if (experience) {
         return (
             <div className="flex flex-1 flex-col items-center justify-center self-center w-full">
                 <EditExperience
@@ -43,17 +44,23 @@ export default async function ExperiencePage({
                 <h1 className="mt-5 md:text-xl font-bold">
                     {experience.title}
                 </h1>
-                <h3 className="mt-3">{experience.date}</h3>
+                <h3 className="mt-3">{experience.date as string}</h3>
                 <div className="relative mt-8 w-full h-auto md:max-w-[67rem] p-4 ">
                     <Carousel
-                        images={experience.detailed.images}
-                        alt={experience.detailed.alt || []}
+                        images={experience.detailed.images as string[]}
+                        alt={(experience.detailed.alt as string[]) || []}
                         path={experience.detailed.rootFolder}
                     ></Carousel>
                 </div>
                 <div className="w-[50%] text-center mt-10">
                     <p>{experience.detailed.description}</p>
                 </div>
+            </div>
+        )
+    } else {
+        return (
+            <div className="w-[90vw] text-center">
+                <h1>Blank page. lol.</h1>
             </div>
         )
     }
