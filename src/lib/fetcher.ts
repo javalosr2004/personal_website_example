353984f6dbe7e690fb4349cd2bef3af9ed8f2159
javalosr2004 <jesusavalosr2004@gmail.com>
2023-08-25 12:35:11 -0700

    added typings for fetcher function

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,3 +1,5 @@
-const fetcher = (...args) => fetch(...args).then((res) => res.json())
+type FetchArgs = [input: RequestInfo, init?: RequestInit]
+
+const fetcher = (...args: FetchArgs) => fetch(...args).then((res) => res.json())
 
 export default fetcher
