7fc11cdcfa3de6f1711dd3a645823d561a434e6c
javalosr2004 <jesusavalosr2004@gmail.com>
2023-08-25 13:13:03 -0700

    testing mongoose directly

--- src/app/components/experience/SimpleBlockLoader.tsx
+++ src/app/components/experience/SimpleBlockLoader.tsx
@@ -2,32 +2,40 @@ import React from 'react'
 import SimpleBlock from './SimpleBlock'
 import { ExperienceState } from '@/store/experienceState'
 import { Skeleton } from '@/components/ui/skeleton'
+import startDB from '@/lib/db'
+import experienceSchema from '@/models/experienceSchema'
+
+// async function getExperiences() {
+//     const DB_URL: string = (process.env.DB_API || '') + '/experiences'
+//     const res = await fetch(DB_URL, {
+//         method: 'GET',
+//         // cache: 'no-store'
+//         next: {
+//             tags: ['experiences'],
+//             revalidate: 3600,
+//         },
+//     })
+//     if (!res.ok) {
+//         return {}
+//     }
+//     const experiences = await res.json()
+//     if (typeof experiences == 'string') {
+//         return JSON.parse(experiences)
+//     } else if (typeof experiences == 'object') {
+//         return experiences
+//     } else {
+//         return {}
+//     }
+// }
 
 async function getExperiences() {
-    const DB_URL: string = (process.env.DB_API || '') + '/experiences'
-    const res = await fetch(DB_URL, {
-        method: 'GET',
-        // cache: 'no-store'
-        next: {
-            tags: ['experiences'],
-            revalidate: 3600,
-        },
-    })
-    if (!res.ok) {
-        return {}
-    }
-    const experiences = await res.json()
-    if (typeof experiences == 'string') {
-        return JSON.parse(experiences)
-    } else if (typeof experiences == 'object') {
-        return experiences
-    } else {
-        return {}
-    }
+    await startDB()
+    const all_experiences: ExperienceState[] = await experienceSchema.find()
+    return all_experiences
 }
 
 export async function SimpleBlockLoader() {
-    const experiences: ExperienceState[] = await getExperiences()
+    const experiences = await getExperiences()
 
     if (!experiences) {
         return (
